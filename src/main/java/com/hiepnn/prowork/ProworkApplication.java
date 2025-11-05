5b7239bb070bece185783b497979a56c8f1a1c1a
hiepnn582 <nnhiep582002@gmail.com>
2025-11-06 00:46:43 +0700

    chore(all): initialize the project structure

---
+++ src/main/java/com/hiepnn/prowork/ProworkApplication.java
@@ -0,0 +1,13 @@
+package com.hiepnn.prowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiepnn/prowork/ProworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiepnn.prowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
